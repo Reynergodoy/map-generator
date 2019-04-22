bff1c20ab4bd8d908a5869f5dc31371f553b90b5
Rey <reyneraccel@gmail.com>
2019-04-21 23:09:02 -0300

    Update random-voronoi.js

--- random-voronoi.js
+++ random-voronoi.js
@@ -17,9 +17,45 @@ class Map {
       }
       this.nodesHandler = true;
     }
+    return this;
   }
   clearNodes () {
     this.nodes = {};
     this.nodesHandler = false;
+    return this;
+  }
+}
+
+class Vector {
+  static intersect (A, B, C, D) {
+    const P = this.subtract(C, D);
+    const r = this.subtract(B, A);
+    const s = this.subtract(D, C);
+    
+    const vPxr = this.vectorProduct(P, r);
+    const vPxs = this.vectorProduct(P, s);
+    const vrxs = this.vectorProduct(r, s);
+    
+    if (!vPxr) return ((C.x - A.x < 0) != (C.x - B.x < 0)) || ((C.y - A.y < 0) != (C.y - B.y < 0));
+ 
+		if (!vrxs) return false;
+ 
+	  const rxsr = 1 / vrxs;
+		const t = vPxs * rxsr;
+		const u = vPxr * rxsr;
+ 
+		return (t >= 0) && (t <= 1) && (u >= 0) && (u <= 1);
+  }
+  static vectorProduct (vectorOne, vectorTwo) {
+    return vectorOne.x * vectorTwo.y - vectorOne.y * vectorTwo.x;
+  }
+  static subtract (pointOne, pointTwo) {
+    return {
+      x: pointOne.x - pointTwo.x,
+      y: pointOne.y - pointTwo.y
+    }
+  }
+  static equality (pointOne, pointTwo) {
+    return ((pointOne.x === pointTwo.x) && (pointOne.y === pointTwo.y));
   }
 }
