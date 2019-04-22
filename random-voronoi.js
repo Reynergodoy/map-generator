b086827610e561db988c476f4a7a27aa1eddd177
Rey <reyneraccel@gmail.com>
2019-04-21 21:57:29 -0300

    Update random-voronoi.js

--- random-voronoi.js
+++ random-voronoi.js
@@ -2,15 +2,23 @@ class Map {
   constructor (length, height) {
     this.dimensions = [length, height];
     this.nodes = {};
+    this.nodesHandler = false;
   }
-  generateNodes () {
-    const nodes = this.nodes;
-    const quantity = this.dimensions.reduce((k, i) => k * i) / 2;
-    for (let i = 0; i < quantity; i++) {
-      nodes[i] = {
-        x: Math.random() * this.dimensions[0],
-        y: Math.random() * this.dimensions[1]
-      };
+  generateNodes (proportion) {
+    if (!this.nodesHandler) {
+      const nodes = this.nodes;
+      const quantity = this.dimensions.reduce((k, i) => k * i) / proportion;
+      for (let i = 0; i < quantity; i++) {
+        nodes[i] = {
+          x: Math.random() * this.dimensions[0],
+          y: Math.random() * this.dimensions[1]
+        };
+      }
+      this.nodesHandler = true;
     }
+  }
+  clearNodes () {
+    this.nodes = {};
+    this.nodesHandler = false;
   }
 }
